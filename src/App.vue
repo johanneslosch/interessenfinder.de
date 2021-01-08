9f8ba431fd6044a8bff40e9dbbdcf6d3538b6c2a
johanneslosch <hi@johanneslosch.com>
2021-01-08 03:04:00 +0100

    added read JSON

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,23 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
+    <li v-for="user in users" :key="user.id">
+      {{user.name}} 
+      <br> 
+      {{user.description}}
+      <br> 
+      Drinnen: {{user.indoor}}
+      <br> 
+      Alleine: {{user.allone}}
+      <br> 
+      <br> 
+    </li>
+<!--
     <HelloWorld msg="Welcome to Your Vue.js App"/>
+    -->
   </div>
 </template>
-
+<!--
 <script>
 import HelloWorld from './components/HelloWorld.vue'
 
@@ -14,6 +27,18 @@ export default {
     HelloWorld
   }
 }
+</script>
+-->
+
+<script>
+import usersData from "./assets/interests.json";
+export default {
+  data() {
+    return {
+      users: usersData,
+      };
+  },
+};
 </script>
 
 <style>
